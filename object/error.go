f1bd06009152e7d36dcc2ba74751a17d2037428c
AN Long <aisk1988@gmail.com>
2025-11-12 00:28:32 +0900

    ✨ Add error object type

---
+++ object/error.go
@@ -0,0 +1,62 @@
+package object
+
+import "fmt"
+
+type Error struct {
+	Value string
+}
+
+func NewError(value string) *Error {
+	return &Error{Value: value}
+}
+
+func (e *Error) Repr() string {
+	return fmt.Sprintf("object.Error(%s)", e.String())
+}
+
+func (e *Error) String() string {
+	return e.Value
+}
+
+func (e *Error) Bool() bool {
+	return true
+}
+
+func (e *Error) Compare(other Object) (int, error) {
+	return 0, ErrNotImplmeneted
+}
+
+func (e *Error) Add(other Object) (Object, error) {
+	return nil, fmt.Errorf("cannot add Error and %T", other)
+}
+
+func (e *Error) Minus(other Object) (Object, error) {
+	return nil, fmt.Errorf("cannot subtract Error and %T", other)
+}
+
+func (e *Error) Multiply(other Object) (Object, error) {
+	return nil, fmt.Errorf("cannot multiply Error and %T", other)
+}
+
+func (e *Error) Divide(other Object) (Object, error) {
+	return nil, fmt.Errorf("cannot divide Error and %T", other)
+}
+
+func (e *Error) And(other Object) (Object, error) {
+	return nil, fmt.Errorf("cannot perform AND operation on Error and %T", other)
+}
+
+func (e *Error) Or(other Object) (Object, error) {
+	return nil, fmt.Errorf("cannot perform OR operation on Error and %T", other)
+}
+
+func (e *Error) Not() (Object, error) {
+	return nil, fmt.Errorf("cannot perform NOT operation on Error")
+}
+
+func (e *Error) Error() string {
+	return e.Value
+}
+
+var _ Object = (*Error)(nil)
+var _ error = (*Error)(nil)
